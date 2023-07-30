a90a28fda259e1d34cbf047da94c5368527ff930
devopstales <42894256+devopstales@users.noreply.github.com>
2023-07-30 16:19:16 +0200

    add aliases folder

---
+++ alias/helm.zsh
@@ -0,0 +1,10 @@
+# helm
+alias hl=helm
+alias hld="helm delete"
+alias hli="helm install"
+alias hls="helm status"
+alias hll="helm list"
+alias hlla="helm list -A"
+alias hlu="helm upgrade"
+alias hlui="helm upgrade --install"
+alias hlun="helm uninstall"
